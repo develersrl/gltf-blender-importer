b693d8ed3c336bc78683c8d6c25d29e5d6f62554
Giorgio Marcias <marcias.giorgio@gmail.com>
2018-03-29 11:45:04 +0200

    principled: combining transparency

--- addons/io_scene_gltf/material_principled.py
+++ addons/io_scene_gltf/material_principled.py
@@ -2,6 +2,14 @@ import bpy
 from io_scene_gltf.material_utils import load_image_from_source
 
 def create_material_from_properties(op, material, material_name):
+    """
+    Create a PBR Material by compbining a 'ShaderNodeBsdfPrincipled' node
+    and, possibly, textures and/ or values declared into 'op'.
+
+    In case of 'alphaMode' not 'OPAQUE', the 'ShaderNodeBsdfPrincipled' node
+    is combined with a 'ShaderNodeBsdfTransparent' node by mixing the result of
+    the former with the output of the latter by the alpha factor defined in 'op'.
+    """
     pbr_mr = material.get('pbrMetallicRoughness', {})
 
     mat = bpy.data.materials.new(material_name)
@@ -16,7 +24,6 @@ def create_material_from_properties(op, material, material_name):
 
     pbr = tree.nodes.new('ShaderNodeBsdfPrincipled')
     mo = tree.nodes.new('ShaderNodeOutputMaterial')
-    links.new(pbr.outputs['BSDF'], mo.inputs['Surface'])
 
     if 'baseColorTexture' in pbr_mr:
         index = pbr_mr['baseColorTexture']['index']
@@ -30,14 +37,26 @@ def create_material_from_properties(op, material, material_name):
 
     alpha_mode = material.get('alphaMode', 'OPAQUE')
     alpha_cutoff = None if alpha_mode != 'MASK' else material.get('alphaCutoff', 0.5)
-    node = _add_color_group(
+    clr = _add_color_group(
         tree,
         base_factor=material.get('baseColorFactor', [1, 1, 1, 1]),
         texture=texture,
         alpha=alpha_mode,
         alpha_cutoff=alpha_cutoff,
     )
-    links.new(node.outputs['Color'], pbr.inputs['Base Color'])
+    links.new(clr.outputs['Color'], pbr.inputs['Base Color'])
+
+    if alpha_cutoff is None:
+        links.new(pbr.outputs['BSDF'], mo.inputs['Surface'])
+    else:
+        trp = tree.nodes.new('ShaderNodeBsdfTransparent')
+        mix = tree.nodes.new('ShaderNodeMixShader')
+        mix.label = "Transparent Mix"
+        tree.links.new(clr.outputs['Alpha Factor'], mix.inputs['Fac'])
+        tree.links.new(trp.outputs['BSDF'], mix.inputs[1])
+        tree.links.new(pbr.outputs['BSDF'], mix.inputs[2])
+        links.new(mix.outputs['Shader'], mo.inputs['Surface'])
+    
     return mat
 
 
@@ -67,27 +86,30 @@ def _add_color_group(material_tree, base_factor, texture=None, alpha='OPAQUE', a
     - if 'MASK' the pixel color is completly opaque if the alpha channel is
       lesser than `alpha_cutoff`, opaque otherwise.
 
-    The return value is a node with two outputs: "Color" and "Alpha".
+    The return value is a node with two outputs: "Color" and "Alpha Factor".
 
     The "Color" output is an RGB ready to be linked to the "Base Color" input
     of a "PrincipledBSDF".
 
-    The "Alpha" output can be used only if the alpha argument is not 'OPAQUE';
+    The "Alpha Factor" output can be used only if the alpha argument is not 'OPAQUE';
     the caller should mix the "BSDF" ouput of a "PrincipledBSDF" with the
-    output of a "TransparentBSDF" using the "Alpha" output as the mix factor.
+    output of a "TransparentBSDF" using the "Alpha Factor" output as the mix factor.
     """
     tree = bpy.data.node_groups.new("Color Group", 'ShaderNodeTree')
-    tree.outputs.new('NodeSocketColor', 'Color')
-    tree.outputs.new('NodeSocketShader', 'Alpha')
+    tree.outputs.new('NodeSocketColor', "Color")
+    tree.outputs["Color"].default_value = base_factor
+    tree.outputs.new('NodeSocketFloatFactor', "Alpha Factor")
+    if alpha is 'OPAQUE':
+        tree.outputs["Alpha Factor"].default_value = 1
+    else:
+        tree.outputs["Alpha Factor"].default_value = base_factor[3]
     outputs = tree.nodes.new('NodeGroupOutput')
 
-    factor = tree.nodes.new('ShaderNodeRGB')
-    factor.label = "Color multiplier"
-    factor.outputs['Color'].default_value = base_factor
+    if texture is not None:
+        factor = tree.nodes.new('ShaderNodeRGB')
+        factor.label = "Color Multiplier"
+        factor.outputs['Color'].default_value = base_factor
 
-    if texture is None:
-        tree.links.new(factor.outputs['Color'], outputs.inputs['Color'])
-    else:
         tex_node = tree.nodes.new('ShaderNodeTexImage')
         tex_node.color_space = 'COLOR'
         tex_node.image = texture['image']
@@ -95,15 +117,52 @@ def _add_color_group(material_tree, base_factor, texture=None, alpha='OPAQUE', a
 
         mixer = tree.nodes.new('ShaderNodeMixRGB')
         mixer.blend_type = 'MULTIPLY'
-        tree.links.new(tex_node.outputs['Color'], mixer.inputs['Color1'])
-        tree.links.new(factor.outputs['Color'], mixer.inputs['Color2'])
+        mixer.inputs["Fac"].default_value = 1
+
+        tree.links.new(factor.outputs['Color'], mixer.inputs['Color1'])
+        tree.links.new(tex_node.outputs['Color'], mixer.inputs['Color2'])
 
         tree.links.new(mixer.outputs['Color'], outputs.inputs['Color'])
+    
+        if alpha is not 'OPAQUE':
+            alpha_node = _create_alpha_factor_pipeline(
+                tree,
+                tex_node.outputs['Alpha'],
+                base_factor[3],
+                alpha_cutoff
+            )
+            tree.links.new(alpha_node.outputs['Value'], outputs.inputs['Alpha Factor'])
+
 
     color_group = material_tree.nodes.new('ShaderNodeGroup')
-    color_group.label = "Material color"
+    color_group.label = "Material Base Color"
     color_group.node_tree = tree
     return color_group
+
+
+def _create_alpha_factor_pipeline(node_tree, tex_alpha_output, base_alpha, alpha_cutoff=None):
+    """
+    Create some nodes computing the final alpha factor by multiplying the base_alpha
+    with the alpha channel of the texture, if any.
+    
+    If alpha_cutoff is not None, than the output alpha factor is either 0 (when
+    the final alpha is less than or equal to alpha_cutoff) or 1 (otherwise)
+    """
+    multiplier = node_tree.nodes.new('ShaderNodeMath')
+    multiplier.label = "Alpha Multiplier"
+    multiplier.operation = 'MULTIPLY'
+    multiplier.inputs[0].default_value = base_alpha
+    node_tree.links.new(tex_alpha_output, multiplier.inputs[1])
+
+    if alpha_cutoff is None:
+        return multiplier
+    
+    cutter = node_tree.nodes.new('ShaderNodeMath')
+    cutter.label = "Cut Off"
+    cutter.operation = 'GREATER_THAN'
+    node_tree.links.new(multiplier.outputs['Value'], cutter.inputs[0])
+    cutter.inputs[1].default_value = alpha_cutoff
+    return cutter
 
 
 _sampler_filter = {
